50a2b37bf0cc6868160808635013e9c1458cde74
Ash Thornton <ashthornton0@gmail.com>
2022-07-10 19:15:51 +0100

    correctly reset body scroll position

--- src/Controller.js
+++ src/Controller.js
@@ -234,6 +234,9 @@ export default class Controller {
 
 		if (store.isTouch && this.options.touchScrollType === 'transform' && this.options.lockIOSBrowserUI) {
 			store.body.style.overflowY = 'auto'
+			if (reset) {
+				document.body.scrollTo(0, 0)
+			}
 		}
 
 		if (restore) {
